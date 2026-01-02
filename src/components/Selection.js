a18dc72b539749b5ca87135298bc65c00dbbed62
Aashish Shilkande <83578134+Aashish257@users.noreply.github.com>
2026-01-02 12:03:32 +0000

    Complete

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,21 @@
+import React, { useState } from 'react';
 
+const Selection = ({ applyColor }) => {
+  const [style, setStyle] = useState({ background: '#eee' });
+
+  const handleClick = () => {
+    applyColor(setStyle);
+  };
+
+  return (
+    <div
+      className="selection"
+      style={style}
+      onClick={handleClick}
+    >
+      Click Me
+    </div>
+  );
+};
+
+export default Selection;
